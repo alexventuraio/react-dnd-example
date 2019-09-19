54791607124541e74a5f6f3cb77acc89919bc0c9
Alejandro Ventura Contreras <soyalexventura@gmail.com>
2019-09-19 01:23:09 -0500

    Convert App into a stateful component to Persist item's order inside the same list. Lesson 5 - Persist List Reordering with react-beautiful-dnd using the onDragEnd Callback.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import { DragDropContext } from 'react-beautiful-dnd';
@@ -7,32 +7,80 @@ import BoardColumn from '../BoardColumn/BoardColumn';
 
 import './App.css';
 
-function App({ data }) {
-  const onDragEnd = () => {
-    // TODO
+class App extends Component {
+  state = this.props.data;
+
+  onDragEnd = result => {
+    /* result = {
+      draggableId: 'task-1',
+      type: 'TYPE',
+      reason: 'DROP',
+      source: {
+        droppableId: 'column-1',
+        inde: 0,
+      },
+      destination: {
+        droppableId: 'column-1',
+        index: 1,
+      },
+    }; */
+    const { destination, source, draggableId } = result;
+
+    if (!destination) return;
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const column = this.state.columns[source.droppableId];
+    const newTaskIds = Array.from(column.taskIds);
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 0, draggableId);
+
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds,
+    };
+
+    const newState = {
+      ...this.state,
+      columns: {
+        ...this.state.columns,
+        [newColumn.id]: newColumn,
+      },
+    };
+
+    this.setState(newState);
   };
 
-  return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <Container fluid>
-        <h1 className="text-white font-weight-light">Kanban Board</h1>
-        <Row className="flex-nowrap scrollable">
-          {data.columnOrder.map((columnId, index) => {
-            const column = data.columns[columnId];
-            const tasks = column.taskIds.map(taskId => data.tasks[taskId]);
-            return (
-              <BoardColumn
-                key={column.id}
-                column={column}
-                tasks={tasks}
-                index={index}
-              />
-            );
-          })}
-        </Row>
-      </Container>
-    </DragDropContext>
-  );
+  render() {
+    return (
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        <Container fluid>
+          <h1 className="text-white font-weight-light">Kanban Board</h1>
+          <Row className="flex-nowrap scrollable">
+            {this.state.columnOrder.map((columnId, index) => {
+              const column = this.state.columns[columnId];
+              const tasks = column.taskIds.map(
+                taskId => this.state.tasks[taskId],
+              );
+              return (
+                <BoardColumn
+                  key={column.id}
+                  column={column}
+                  tasks={tasks}
+                  index={index}
+                />
+              );
+            })}
+          </Row>
+        </Container>
+      </DragDropContext>
+    );
+  }
 }
 
 export default App;
