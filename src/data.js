d21f5e13d84f6812b0dce75ab47d45cd5ab8780d
Alejandro Ventura Contreras <soyalexventura@gmail.com>
2019-09-20 11:17:41 -0500

    Add a few more columns and tasks to validate scrollable behavior horizontally and vertically.

--- src/data.js
+++ src/data.js
@@ -4,12 +4,29 @@ const initialData = {
     'task-2': { id: 'task-2', content: 'Watch my favorite show' },
     'task-3': { id: 'task-3', content: 'Charge my phone' },
     'task-4': { id: 'task-4', content: 'Cook diner' },
+    'task-5': { id: 'task-5', content: 'Submit a Pull Request up to date.' },
+    'task-6': { id: 'task-6', content: 'Submit a Pull Request up to date.' },
+    'task-7': { id: 'task-7', content: 'Submit a Pull Request up to date.' },
+    'task-8': { id: 'task-8', content: 'Submit a Pull Request up to date.' },
+    'task-9': { id: 'task-9', content: 'Submit a Pull Request up to date.' },
+    'task-10': { id: 'task-10', content: 'Submit a Pull Request up to date.' },
   },
   columns: {
     'column-1': {
       id: 'column-1',
       title: 'To Do',
-      taskIds: ['task-1', 'task-2', 'task-3', 'task-4'],
+      taskIds: [
+        'task-1',
+        'task-2',
+        'task-3',
+        'task-4',
+        'task-5',
+        'task-6',
+        'task-7',
+        'task-8',
+        'task-9',
+        'task-10',
+      ],
     },
     'column-2': {
       id: 'column-2',
@@ -21,9 +38,19 @@ const initialData = {
       title: 'Done',
       taskIds: [],
     },
+    'column-4': {
+      id: 'column-4',
+      title: 'Deploy',
+      taskIds: [],
+    },
+    'column-5': {
+      id: 'column-5',
+      title: 'POA',
+      taskIds: [],
+    },
   },
   // Facilitate reordering of the columns
-  columnOrder: ['column-1', 'column-2', 'column-3'],
+  columnOrder: ['column-1', 'column-2', 'column-3', 'column-4', 'column-5'],
 };
 
 export default initialData;
