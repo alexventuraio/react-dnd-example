d682d68f957589dd6b4f77744a6aa18e51b044bc
Alejandro Ventura Contreras <venturacontreras@icloud.com>
2019-09-19 20:03:55 -0500

    First approach to prevent from triggering unnecessary render methods when dragging a card outside its column.

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,22 +1,24 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import Task from '../Task/Task';
 
 import './TaskList.css';
 
 let counter = 0;
 
-const TaskList = ({ tasks, columnId }) => {
+class TaskList extends PureComponent {
   /*droppableSnapshot = {
     isDraggingOver: true,
     draggingOverWith: 'task-1',
   };*/
 
-  return tasks.map((task, index) => {
-    counter++;
-    console.count('Render Component');
-    console.log('Counter: ', counter);
-    return <Task key={task.id} task={task} index={index} />;
-  });
-};
+  render() {
+    return this.props.tasks.map((task, index) => {
+      counter++;
+      console.count('Render Component');
+      console.log('Counter: ', counter);
+      return <Task key={task.id} task={task} index={index} />;
+    });
+  }
+}
 
 export default TaskList;
