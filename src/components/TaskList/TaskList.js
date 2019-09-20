bd798ad30f882edaf825e65f4e7635a34b880760
Alejandro Ventura Contreras <soyalexventura@gmail.com>
2019-09-19 19:55:03 -0500

    Implement a way to show the number of 'renders' we are triggering on each card/column drag action.

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -3,15 +3,20 @@ import Task from '../Task/Task';
 
 import './TaskList.css';
 
+let counter = 0;
+
 const TaskList = ({ tasks, columnId }) => {
   /*droppableSnapshot = {
     isDraggingOver: true,
     draggingOverWith: 'task-1',
   };*/
 
-  return tasks.map((task, index) => (
-    <Task key={task.id} task={task} index={index} />
-  ));
+  return tasks.map((task, index) => {
+    counter++;
+    console.count('Render Component');
+    console.log('Counter: ', counter);
+    return <Task key={task.id} task={task} index={index} />;
+  });
 };
 
 export default TaskList;
